33350d8accfe97affdceed30228c55bc08267bd4
yonathan <yonatanalebachew7@gmail.com>
2024-11-16 16:01:45 +0300

    profilepicture saved

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDy9QG45FkaGxcp_3k9GqdxjKafajiGKyA",
-  authDomain: "digital-menu-47c30.firebaseapp.com",
-  projectId: "digital-menu-47c30",
-  storageBucket: "digital-menu-47c30.appspot.com",
-  messagingSenderId: "350396316425",
-  appId: "1:350396316425:web:d9ba842b742e4537d431d8",
-  measurementId: "G-MTFRTKGPVP"
+  
 };
 
 
